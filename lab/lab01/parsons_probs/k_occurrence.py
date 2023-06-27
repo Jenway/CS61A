e6ec94833ca1c605a014144fc74bd676711ca854
Jenway <JenwayHere@gmail.com>
2023-06-27 23:38:31 +0800

    week2 done :) 🦋

---
+++ lab/lab01/parsons_probs/k_occurrence.py
@@ -0,0 +1,18 @@
+def k_occurrence(k, num):
+    """
+    >>> k_occurrence(5, 10)  # .Case 1
+    0
+    >>> k_occurrence(5, 5115)  # .Case 2
+    2
+    >>> k_occurrence(0, 100)  # .Case 3
+    2
+    >>> k_occurrence(0, 0)  # .Case 4
+    0
+    """
+    "*** YOUR CODE HERE ***"
+    times = 0
+    while num != 0:
+        if k == int(num%10):
+            times+=1
+        nums /= 10
+    return times
